455802d2f06bbf90c0c0b8d96a5c8c1ef77ce22e
c-bata <contact@c-bata.link>
2019-03-19 14:44:06 +0900

    Define struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
+use bit_vec::BitVec;
+
+#[derive(Debug)]
+pub struct BloomFilter {
+    pub buf: BitVec
+}
+
+impl BloomFilter {
+    pub fn new(m: usize) -> BloomFilter {
+        let buf = BitVec::from_elem(m, false);
+
+        BloomFilter {
+            buf,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::BloomFilter;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let bloom = BloomFilter::new(4);
+        println!("bloom: {:?}", bloom);
+        assert_eq!(bloom.buf.len(), 4);
     }
 }
