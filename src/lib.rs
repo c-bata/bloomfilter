9d90a99d77d09b1a2f1b2cf61e79128f8ef8fb68
c-bata <contact@c-bata.link>
2019-03-19 15:18:21 +0900

    Implement bloom filter

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,14 @@ pub struct BloomFilter {
     pub buf: BitVec
 }
 
+fn h1(key: usize) -> usize {
+    return key % 8
+}
+
+fn h2(key: usize) -> usize {
+    return (key * 2) % 8
+}
+
 impl BloomFilter {
     pub fn new(m: usize) -> BloomFilter {
         let buf = BitVec::from_elem(m, false);
@@ -13,6 +21,27 @@ impl BloomFilter {
             buf,
         }
     }
+
+    pub fn set(&mut self, key: usize) {
+        let a = h1(key);
+        let b = h2(key);
+        self.buf.set(a, true);
+        self.buf.set(b, true);
+    }
+
+    pub fn is_exit(&self, key: usize) -> bool {
+        let h1_bit: bool;
+        match self.buf.get(h1(key)) {
+            Some(x) => h1_bit = x,
+            None => return false
+        }
+        let h2_bit: bool;
+        match self.buf.get(h2(key)) {
+            Some(x) => h2_bit = x,
+            None => return false
+        }
+        return h1_bit && h2_bit
+    }
 }
 
 #[cfg(test)]
@@ -20,9 +49,20 @@ mod tests {
     use crate::BloomFilter;
 
     #[test]
-    fn it_works() {
-        let bloom = BloomFilter::new(4);
-        println!("bloom: {:?}", bloom);
-        assert_eq!(bloom.buf.len(), 4);
+    fn test_set_bit() {
+        let mut bloom = BloomFilter::new(16);
+        bloom.set(1000);
+        assert_eq!(bloom.is_exit(1000), true);
+        assert_eq!(bloom.is_exit(1001), false);
+    }
+
+    #[test]
+    fn test_false_positive() {
+        let mut bloom = BloomFilter::new(16);
+        bloom.set(1000);
+        bloom.set(1001);
+        bloom.set(1004);
+        assert_eq!(bloom.is_exit(1005), false);
+        assert_eq!(bloom.is_exit(1020), true);
     }
 }
